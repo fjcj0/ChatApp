17fe7c24338c64462836d5c957eaed1a2bdf2a6c
omar <omarqais225@gmail.com>
2025-03-03 00:45:21 +0200

    connect logout with frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import { axiosInstance } from '../../lib/axios.js';
 import { toast } from 'react-hot-toast';
+import axios from 'axios';
 export const useAuthStore = create((set) => ({
     authUser: null,
     isSigningUp: false,
@@ -30,5 +31,14 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isSigningUp: false });
         }
-    }
+    },
+    logout: async () => {
+        try {
+            await axiosInstance.post('/auth/logout');
+            set({ authUser: null });
+            toast.success('logout successfully!!');
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    },
 }));
