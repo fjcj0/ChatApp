a7aeeb5d975cd82a15f3f0b28eb715d8ce88ab4e
omar <omarqais225@gmail.com>
2025-03-09 03:24:50 +0200

    fix problems

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,6 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app, server, io } from './lib/socket.js';
-import path from 'path';
-
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -26,13 +22,8 @@ app.options('*', cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-    app.get('*', (request, response) => {
-        response.sendFile(path.join(__dirname, '../../frontend', "dist", "index.html"));
-    });
-}
 
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
     console.log(`App listening on port ${PORT} 🚀`);
