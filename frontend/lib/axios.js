01f26e989e56998bbe57e1dd516be135f1c67768
omar <omarqais225@gmail.com>
2025-03-09 16:04:12 +0200

    change domain

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://backend-chat-app-three.vercel.app/api',
     withCredentials: true,
 });
