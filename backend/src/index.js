ce165eede66bb31670a60f8326868dfbd73f4693
omar <omarqais225@gmail.com>
2025-03-09 16:27:46 +0200

    try to deploy again

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,9 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app, server, io } from './lib/socket.js';
+import path from 'path';
+const __dirname = path.resolve();
+console.log(__dirname);
 dotenv.config();
 
 app.use(express.json());
@@ -21,7 +24,13 @@ app.options('*', cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
